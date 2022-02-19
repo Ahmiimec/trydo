ab1928b10e7a14110da8ec0883629a5749b6fccd
Ahmed Jamil <ahmed.jamil@creditfix.pk>
2022-02-20 00:08:44 +0500

    Change title

--- src/dark/MainDemo.jsx
+++ src/dark/MainDemo.jsx
@@ -19,7 +19,7 @@ class MainDemo extends Component{
         const PostList = BlogContent.slice(0 , 3);
         return(
             <div className="active-dark"> 
-                <Helmet pageTitle="| Lets Blend It |" />
+                <Helmet pageTitle="Home" />
                 <Header headertransparent="header--transparent" colorblack="color--black" logoname="logo.png" />
 
                 {/* Start Slider Area   */}
